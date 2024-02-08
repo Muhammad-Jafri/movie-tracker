3612991bc289ae63aaac2f29a90e588ab2f4f091
Muhammad <24100113@lums.edu.pk>
2024-02-09 01:47:11 +0500

    seed more movies

--- scripts/placeholder-data.js
+++ scripts/placeholder-data.js
@@ -1,16 +1,52 @@
 
-movies = [
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Shadows of Tomorrow", release_date: "2024-06-15", genre: "Science Fiction"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Beyond the Horizon", release_date: "2025-12-22", genre: "Adventure"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Echoes of the Unknown", release_date: "2023-11-05", genre: "Mystery"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Guardians of the Abyss", release_date: "2024-03-18", genre: "Fantasy"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Last Chronicle", release_date: "2025-09-10", genre: "Drama"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "A Whisper in the Void", release_date: "2023-08-21", genre: "Horror"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Rifts in Time", release_date: "2024-01-30", genre: "Time Travel"},
-    {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Dreamweaver's Quest", release_date: "2025-07-20", genre: "Fantasy"}
-]
+// movies = [
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Shadows of Tomorrow", release_date: "2024-06-15", genre: "Science Fiction"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Beyond the Horizon", release_date: "2025-12-22", genre: "Adventure"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Echoes of the Unknown", release_date: "2023-11-05", genre: "Mystery"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Guardians of the Abyss", release_date: "2024-03-18", genre: "Fantasy"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Last Chronicle", release_date: "2025-09-10", genre: "Drama"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "A Whisper in the Void", release_date: "2023-08-21", genre: "Horror"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "Rifts in Time", release_date: "2024-01-30", genre: "Time Travel"},
+//     {image : 'https://fastly.picsum.photos/id/884/200/300.jpg?hmac=VnWK-J-znCMSx2FSelz3LtT1DXhrxRLtzsX6-hkZDJk' ,name: "The Dreamweaver's Quest", release_date: "2025-07-20", genre: "Fantasy"}
+// ]
+
+img = 'https://fastly.picsum.photos/id/379/200/300.jpg?hmac=IEcRQyv-DIaRsldE8jIdMRW5IHCTyemefU-bbCJpY34'
+
+const movies = [
+  {
+    image : img,
+    name: "Journey Through the Echoes",
+    release_date: "2025-03-15",
+    genre: "Science Fiction"
+  },
+  {
+    image : img,
+    name: "The Last Painter of Dreams",
+    release_date: "2024-11-22",
+    genre: "Drama"
+  },
+  {
+    image : img,
+    name: "Whispers of the Ancient",
+    release_date: "2025-07-08",
+    genre: "Fantasy"
+  },
+  {
+    image : img,
+    name: "Code of the Forgotten",
+    release_date: "2026-02-19",
+    genre: "Thriller"
+  },
+  {
+    image : img,
+    name: "Shadows Over Crimson Tides",
+    release_date: "2024-12-25",
+    genre: "Horror"
+  }
+];
 
 
-console.log(movies.length);
+
+
 
 module.exports = movies;
